8529585c8981ba8324f15169e34187c6a6a90be7
David Cediel <esdago18@gmail.com>
2020-02-10 12:22:56 -0500

    Mi primer commit

---
+++ js/miJS.js
@@ -0,0 +1,6 @@
+
+function volverBlanco(){
+    caja_texto.style.backgroundColor = "white";
+}
+var caja_texto = document.querySelector("#volverBlanco");
+caja_texto.addEventListener("click", volverBlanco, false);
