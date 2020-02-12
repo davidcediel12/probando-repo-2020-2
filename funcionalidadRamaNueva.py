d8e2f7fc543a69299fc8a05412d04162beff6c50
David Cediel <esdago18@gmail.com>
2020-02-12 12:50:17 -0500

    me perdi

--- funcionalidadRamaNueva.py
+++ funcionalidadRamaNueva.py
@@ -2,10 +2,7 @@
 def funct(*args, **kwargs):
     print("Esto lo hice en la rama master")
     print("Esto tambien")
-<<<<<<< HEAD
     print("Esto lo hice en la rama master luego de" + 
             "modificar el archivo en otra rama")
-=======
 
     print("esto lo hice en la nueva rama")
->>>>>>> nuevaRama
