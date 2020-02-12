d6a19c8dacbf18d2b55363593b6e096fd3a57ec7
David Cediel <esdago18@gmail.com>
2020-02-12 12:36:53 -0500

    Se agrego otro print al archivo funcionalidadRamaNueva.py

--- funcionalidadRamaNueva.py
+++ funcionalidadRamaNueva.py
@@ -2,3 +2,5 @@
 def funct(*args, **kwargs):
     print("Esto lo hice en la rama master")
     print("Esto tambien")
+    print("Esto lo hice en la rama master luego de" + 
+            "modificar el archivo en otra rama")
